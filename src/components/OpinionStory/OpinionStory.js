2703303707bc272f3dfe8c2439bd6c074e11005a
Fede Alvarez <fede.alvarez.velasco@gmail.com>
2021-12-21 07:15:16 -0600

    Made avatars to float right and adjusted media queries (tabletAndUp, laptopAndUp) to match design.

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { QUERIES } from '../../constants';
+
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
     <a href={`/story/${id}`}>
@@ -25,6 +27,18 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  float: right;
+  margin-left: 16px;
+
+  @media ${QUERIES.tabletAndUp} {
+    float: revert;
+    margin-left: revert;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    float: right;
+    margin-left: 16px;
+  }
 `;
 
 const AuthorName = styled.p`
